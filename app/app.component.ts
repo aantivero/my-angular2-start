052af2b45acea17f47bbba8670e70a2543f97bef
Alejandro Antivero <alejandro.antivero@gmail.com>
2016-04-29 00:02:53 -0300

    Tutorial Pt2: Array of Objects

--- app/app.component.ts
+++ app/app.component.ts
@@ -22,4 +22,18 @@ export class AppComponent {
         id: 1,
         name: 'windstorm'
     };
+    public heroes = HEROES;
 }
+
+var HEROES: Hero[] = [
+    {"id": 11, "name": "Mr. Nice"},
+    {"id": 12, "name": "Marco"},
+    {"id": 13, "name": "Bombasto"},
+    {"id": 14, "name": "Celeritas"},
+    {"id": 15, "name": "Magneta"},
+    {"id": 16, "name": "Ruber Man"},
+    {"id": 17, "name": "Dynama"},
+    {"id": 18, "name": "Dr IQ"},
+    {"id": 19, "name": "Magma"},
+    {"id": 20, "name": "Tornado"}
+];
