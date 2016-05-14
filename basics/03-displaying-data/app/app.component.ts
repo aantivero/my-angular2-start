cf64a9788947ec6a996b7407094271c654b9f6a7
Alejandro Antivero <alejandro.antivero@gmail.com>
2016-05-14 15:55:03 -0300

    Utilizar el constructor

--- basics/03-displaying-data/app/app.component.ts
+++ basics/03-displaying-data/app/app.component.ts
@@ -6,6 +6,11 @@ import {Component} from '@angular/core';
     <h2>my favorite hero is: {{myHero}}</h2>`
 })
 export class AppComponent {
-    title = 'Tour of Heroes';
-    myHero = 'windstorm';
+    title: string;
+    myHero: string;
+    
+    constructor() {
+        this.title = 'Tour of Heroes';
+        this.myHero = 'windstorm';
+    }
  }
