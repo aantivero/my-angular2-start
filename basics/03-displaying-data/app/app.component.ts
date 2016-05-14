03cfea84f54256d63688a4610b045805c96555bd
Alejandro Antivero <alejandro.antivero@gmail.com>
2016-05-14 16:00:49 -0300

    Iterar por el array con ngFor

--- basics/03-displaying-data/app/app.component.ts
+++ basics/03-displaying-data/app/app.component.ts
@@ -3,14 +3,23 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `<h1>{{title}}</h1>
-    <h2>my favorite hero is: {{myHero}}</h2>`
+    <h2>my favorite hero is: {{myHero}}</h2>
+    <p>Heroes</p>
+    <ul>
+        <li *ngFor="let hero of heroes">
+            {{hero}}
+        </li>
+    </ul>
+    `
 })
 export class AppComponent {
     title: string;
+    heroes: Array<string>;
     myHero: string;
     
     constructor() {
         this.title = 'Tour of Heroes';
-        this.myHero = 'windstorm';
+        this.heroes = ['windstorm', 'bombasto', 'magneta', 'tornado'];
+        this.myHero = this.heroes[0];
     }
  }
