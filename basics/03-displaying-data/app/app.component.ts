0b541df8e37f540875f228005732f22e3d57f432
Alejandro Antivero <alejandro.antivero@gmail.com>
2016-05-14 16:09:05 -0300

    Crear una clase e iterar sobre los objetos del array.

--- basics/03-displaying-data/app/app.component.ts
+++ basics/03-displaying-data/app/app.component.ts
@@ -1,25 +1,30 @@
 import {Component} from '@angular/core';
+import {Hero} from './hero';
 
 @Component({
     selector: 'my-app',
     template: `<h1>{{title}}</h1>
-    <h2>my favorite hero is: {{myHero}}</h2>
+    <h2>my favorite hero is: {{myHero.name}}</h2>
     <p>Heroes</p>
     <ul>
         <li *ngFor="let hero of heroes">
-            {{hero}}
+            {{hero.name}}
         </li>
     </ul>
     `
 })
 export class AppComponent {
     title: string;
-    heroes: Array<string>;
-    myHero: string;
+    heroes: Array<Hero>;
+    myHero: Hero;
     
     constructor() {
         this.title = 'Tour of Heroes';
-        this.heroes = ['windstorm', 'bombasto', 'magneta', 'tornado'];
+        this.heroes = [
+            new Hero(1, 'windstorm'), 
+            new Hero(1,'bombasto'), 
+            new Hero(1, 'magneta'), 
+            new Hero(1,'tornado')];
         this.myHero = this.heroes[0];
     }
  }
