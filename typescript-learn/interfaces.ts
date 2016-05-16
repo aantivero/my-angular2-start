26bbc581de8ca773ff3621d7c31e68352a8a58be
Alejandro Antivero <alejandro.antivero@gmail.com>
2016-05-15 23:18:00 -0300

    Interfaces: comienzo con algo sencillo.

---
+++ typescript-learn/interfaces.ts
@@ -0,0 +1,8 @@
+//primera interfaz
+function imprimirLabel(labelledObject: {label: string}) {
+    console.log(labelledObject.label);
+}
+
+let primerObj = {size: 10, label: "size 10 object"};
+imprimirLabel(primerObj);
+
