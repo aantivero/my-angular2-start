f1af44d5b094ac8561b7db9812991cfb12a42d07
Alejandro Antivero <alejandro.antivero@gmail.com>
2016-05-15 23:36:54 -0300

    Interface Excess property checks

--- typescript-learn/interfaces.ts
+++ typescript-learn/interfaces.ts
@@ -13,8 +13,8 @@ imprimirLabel(primerObj);
 //propiedades opcionales porque no todas pueden ser requeridas
 interface SquareConfig {
     color?: string;
-    
     width?: number;
+    [propName: string]: any;
 }
 
 function crearSquare(config: SquareConfig): {color: string; area: number} {
@@ -28,3 +28,8 @@ function crearSquare(config: SquareConfig): {color: string; area: number} {
     return newSquare;
 }
 let micuadrado = crearSquare({color: "negro"});
+//excess property checks
+let otrocuadrado = crearSquare({colour: "rojo", width: 1000} as SquareConfig);
+
+let squareOptions = {colour: "red", width: 300};
+let miocuadrado = crearSquare(squareOptions);
