5cfeb76a2334d83f182d1ffda1fb4579094c6ade
Alejandro Antivero <alejandro.antivero@gmail.com>
2016-05-15 23:27:11 -0300

    Interfaces: propiedades opcionales

--- typescript-learn/interfaces.ts
+++ typescript-learn/interfaces.ts
@@ -10,3 +10,21 @@ function imprimirLabel(labelledObject: LabelledValue) {
 let primerObj = {size: 10, label: "size 10 object"};
 imprimirLabel(primerObj);
 
+//propiedades opcionales porque no todas pueden ser requeridas
+interface SquareConfig {
+    color?: string;
+    
+    width?: number;
+}
+
+function crearSquare(config: SquareConfig): {color: string; area: number} {
+    let newSquare = {color: "white", area: 100};
+    if (config.color) {
+        newSquare.color = config.color;
+    }
+    if (config.width) {
+        newSquare.area = config.width * config.width;
+    }
+    return newSquare;
+}
+let micuadrado = crearSquare({color: "negro"});
