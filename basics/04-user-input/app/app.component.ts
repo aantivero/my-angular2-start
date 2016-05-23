af914dd2d5d3bbbd1c5fbeac320a6a7d27584131
Alejandro Antivero <alejandro.antivero@gmail.com>
2016-05-23 07:17:07 -0300

    Capturando keyup event de un input box

--- basics/04-user-input/app/app.component.ts
+++ basics/04-user-input/app/app.component.ts
@@ -1,11 +1,13 @@
 import {Component} from '@angular/core';
 import {ClickMeComponent} from './click-me.component';
+import {KeyUpComponent_v1} from './keyup.components';
 
 @Component({
     selector: 'my-app',
     templateUrl: 'app/app.component.html',
     directives: [
-        ClickMeComponent
+        ClickMeComponent,
+        KeyUpComponent_v1
     ]
 })
 export class AppComponent { }
