4472fed554d4e4ae7ed9c21e710403f9d971c76a
Alejandro Antivero <alejandro.antivero@gmail.com>
2016-05-23 01:15:53 -0300

    Binding to user input events

--- basics/04-user-input/app/app.component.ts
+++ basics/04-user-input/app/app.component.ts
@@ -1,7 +1,12 @@
 import {Component} from '@angular/core';
+import {ClickMeComponent} from './click-me.component';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `<h1>My First Angular 2 App</h1>
+    <p><click-me></click-me></p>`,
+    directives: [
+        ClickMeComponent
+    ]
 })
 export class AppComponent { }
